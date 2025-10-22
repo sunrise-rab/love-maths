04acd5b9cb2c9f6b9d258afd1f6196629456968c
sunrise-rab <rabia.tsdi@gmail.com>
2025-10-22 13:33:59 +0100

    Add multiply game and responce to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,9 +31,11 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1,num2);
+    } else{
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
+      throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
@@ -72,7 +74,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator==="x"){
+        return [operand1 * operand2, "multiply"];
+    } {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -111,6 +115,9 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
