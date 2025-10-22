5fc45e97d446faf2062c401fa6b6085f55f93b95
sunrise-rab <rabia.tsdi@gmail.com>
2025-10-22 20:09:33 +0100

    Add substract game and response to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,9 +33,11 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1,num2);
-    } else{
+    } else if (gameType === "substract"){
+        displaySubtractQuestion(num1,num2);
+    }else{
         alert(`Unknown game type: ${gameType}`);
-      throw `Unknown game type: ${gameType}. Aborting!`;
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
@@ -74,9 +76,11 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else if (operator==="x"){
+    } else if (operator=== "x" ){
         return [operand1 * operand2, "multiply"];
-    } {
+    } else if (operator=== "-" ){
+        return [operand1 - operand2, "substract"];
+    }else { 
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -111,7 +115,10 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
